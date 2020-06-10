60dbd5cbb6597ffa1e3d5f0af7b1b54cb76dcc34
Spencer Moody <spencer.moody@outlook.com>
2020-06-10 15:35:15 -0700

    Add output move consonants to end and add ay
    Co-authored-by: Kate Skorija <kateskorija@gmail.com>
    Co-authored-by: Matthew Craig <mgcraig78@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
     event.preventDefault();
     
     const userInput = $("input#entry").val();
-    const vowelsArray = ["a", "e", "i", "o", "u"]
+    const vowelsArray = ["a", "e", "i", "o", "u", "y"];
+    const consonantsArray = ["b", "c", "d", "f", "g", "h", "k", "l", "m", "n", "p", "r", "s", "t", "v", "w", "x", "z"];
     let pigArr = [];
 
     function isLetter(userInput) { 
@@ -18,22 +19,34 @@ $(document).ready(function(){
       if(vowelsArray.includes(word[0])){
         word = word + "way"
         pigArr.push(word)
-        console.log(pigArr)
       } else if (word[0]==="q" && word[1]==="u"){
         word = word + word[0] + word[1] + "ay"
         word = word.substr(2,word.length-1)
         pigArr.push(word);
-        console.log(pigArr);
-      }
-      else if (word[0]==="s" && word [1]==="q" && word[2]==="u"){
+      } else if (word[0]==="s" && word [1]==="q" && word[2]==="u"){
         word = word + word[0] + word[1] + word[2] + "ay"
         word = word.substr(3,word.length-1)
         pigArr.push(word);
-        console.log(pigArr);
+      } else {
+        let newArray = userInput.split("");
+        let count = 0; 
+        for(let i = 0; i <= 3; ++i){
+          if(consonantsArray.includes(newArray[i]))
+          count++
+          word = newArray.join("");
+          word = word + word.substr(count,word.length-1) + "ay"
+          pigArr.push(word);
+          break;
+      }
+        // word = word + word[0] + "ay"
+        // word = word.substr(1, word.length-1)
+        // pigArr.push(word);
+        // console.log(pigArr);
       }
       
+      $("#output").text(pigArr.join(" "));
     }); 
     })
   })
-
-
+  
+  
