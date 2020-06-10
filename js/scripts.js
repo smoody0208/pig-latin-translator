c2990c7ee3c31b6fb86011fca801c7ba1c6a7371
Spencer Moody <spencer.moody@outlook.com>
2020-06-10 14:41:52 -0700

    Add functionality to move 'qu' to end of word and add 'ay' onto the end
    Co-authored-by: Kate Skorija <kateskorija@gmail.com>
    Co-authored-by: Matthew Craig <mgcraig78@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,39 @@
 $(document).ready(function(){
   $("form#input").submit(function(event){
     event.preventDefault();
+    
     const userInput = $("input#entry").val();
-    const constArray = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
     const vowelsArray = ["a", "e", "i", "o", "u"]
+    let pigArr = [];
 
-    function isLetter(userInput) {
+    function isLetter(userInput) { 
       if (!isNaN(userInput)) {
         alert("Please enter a sentence");
-     
       }
     }
     isLetter(userInput);
+
+    const arrStr = userInput.split(" ");
+    arrStr.forEach(function(word){
+      if(vowelsArray.includes(word[0])){
+        word = word + "way"
+        pigArr.push(word)
+        console.log(pigArr)
+      } else if (word[0]==="q" && word[1]==="u"){
+        word = word + word[0] + word[1] + "ay"
+        word = word.substr(2,word.length-1)
+        pigArr.push(word);
+        console.log(pigArr);
+      }
+      else if (word[0]==="s" && word [1]==="q" && word[2]==="u"){
+        word = word + word[0] + word[1] + word[2] + "ay"
+        word = word.substr(3,word.length-1)
+        pigArr.push(word);
+        console.log(pigArr);
+      }
+      
+    }); 
+    })
   })
-})
-let pigArr = [];
 
 
